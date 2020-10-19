c2a99d4ad7753ddac323c2e41c472f14748ea6e2
ramjinish kushwaha <70768729+ramjinish@users.noreply.github.com>
2020-10-19 11:55:36 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home.dart';
+import 'plugins/desktop/desktop.dart';
+
+void main() {
+  // Stablishes target platform if neccessary (desktop only)
+  setTargetPlatform();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Everywhere',
+      home: HomePage(),
+    );
+  }
+}
